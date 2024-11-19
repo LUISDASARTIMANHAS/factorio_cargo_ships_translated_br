f10af873e657040f0ba71e1bc03f7f0559d4b2f2
Tom Burrows <tomburrows13@icloud.com>
2024-11-19 02:52:30 +0000

    Add commands - not really useful for changing existing chunks other than enabling

--- control.lua
+++ control.lua
@@ -547,11 +547,97 @@ script.on_configuration_changed(function()
   init()
 end)
 
+local function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
 
 -- Console commands
 commands.add_command("cargo-ships-dump", "Dump storage to log", function() log(serpent.block(storage)) end)
 
+local usage_string_get = "Usage: /cargo-ships-get-oil-settings <planet>"
+commands.add_command("cargo-ships-get-oil-settings", "Get oil settings\n" .. usage_string_get, function(command)
+  if not command.parameter or not game.planets[command.parameter] then
+    game.print(usage_string_get)
+    return
+  end
+  local planet = game.planets[command.parameter]
+  if not planet.surface then
+    game.print("Planet " .. planet.name .. " has not been generated yet")
+    return
+  end
+  local crude_control_name = "crude-oil"
+  local offshore_control_name = "offshore-oil"
+  if planet.name == "aquilo" then
+    crude_control_name = "aquilo_crude_oil"
+    offshore_control_name = "aquilo_offshore_oil"
+  end
+  local map_gen_settings = planet.surface.map_gen_settings
+  local crude_autoplace_controls = map_gen_settings.autoplace_controls[crude_control_name]
+  game.print(planet.name .. " crude-oil settings: " .. serpent.line(crude_autoplace_controls))
+  local offshore_autoplace_controls = map_gen_settings.autoplace_controls[offshore_control_name]
+  game.print(planet.name .. " offshore-oil settings: " .. serpent.line(offshore_autoplace_controls))
+end)
 
+local usage_string = "Usage: /cargo-ships-set-oil-settings <planet> <offshore-oil/crude-oil> <default/off/{frequency=X,richness=Y,size=Z}>"
+commands.add_command("cargo-ships-set-oil-settings", "Set oil configuration\n" .. usage_string, function(command)
+  if not command.parameter then
+    game.print(usage_string)
+    return
+  end
+  local params = split(command.parameter, " ")
+  if #params ~= 3 then
+    game.print("Wrong number of parameters")
+    game.print(usage_string)
+    return
+  end
+  local planet_name = params[1]
+  local resource_name = params[2]
+  local settings_string = params[3]
+
+  if not game.planets[planet_name] then
+    game.print("Planet " .. planet_name .. " does not exist")
+    return
+  end
+  local planet = game.planets[planet_name]
+  if not planet.surface then
+    game.print("Planet " .. planet_name .. " has not been generated yet")
+    return
+  end
+  local control_name = "offshore-oil"
+  if planet_name == "aquilo" then
+    control_name = "aquilo_offshore_oil"
+  end
+  local map_gen_settings = planet.surface.map_gen_settings
+
+  if settings_string == "default" then
+    map_gen_settings.autoplace_controls[control_name] = {}
+  elseif settings_string == "off" then
+    map_gen_settings.autoplace_controls[control_name] = {frequency = 1, size = 0, richness = 1}
+  else
+    local ok, res = serpent.load(settings_string)
+    if not ok then
+      game.print("Error parsing settings: " .. settings_string)
+      return
+    end
+    map_gen_settings.autoplace_controls[control_name] = res
+  end
+  planet.surface.map_gen_settings = map_gen_settings
+  --[[local entities = planet.surface.find_entities_filtered{type="resource", name=resource_name}
+  for _, entity in pairs(entities) do
+    entity.destroy()
+  end]]
+
+  -- Doesn't delete entities, doesn't create entities if they've already been autoplaced
+  planet.surface.regenerate_entity(resource_name)
+  game.print("Set " .. planet_name .. " " .. resource_name .. " settings to " .. settings_string)
+end)
 ------------------------------------------------------------------------------------
 --                    FIND LOCAL VARIABLES THAT ARE USED GLOBALLY                 --
 --                              (Thanks to eradicator!)                           --
@@ -561,9 +647,9 @@ setmetatable(_ENV,{
     error('\n\n[ER Global Lock] Forbidden global *write*:\n'
       .. serpent.line{key=key or '<nil>',value=value or '<nil>'}..'\n')
     end,
-  __index   =function (self,key) --locked_global_read
+  --[[__index   =function (self,key) --locked_global_read
     error('\n\n[ER Global Lock] Forbidden global *read*:\n'
       .. serpent.line{key=key or '<nil>'}..'\n')
-    end ,
+    end ,]]
   })
 
